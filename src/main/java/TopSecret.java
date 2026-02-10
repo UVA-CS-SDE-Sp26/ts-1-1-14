62312e152589d2d14a8e94c299e17d09f8ff43f4
AshishSunkarapalli <ashishs5709@gmail.com>
2026-02-09 22:20:52 -0500

    Created the basic outline of the command line code.

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -2,4 +2,29 @@
  * Commmand Line Utility
  */
 public class TopSecret {
+
+    public static void main(String[] args) {
+
+        if(args.length = 0) {
+            System.out.println("Listing all files...");
+            //Call Lamin's list method later
+        }
+
+        else if (args.length == 1) {
+            String fileName = args[0];
+            System.out.println("Reading file name: " + fileName);
+            //Call Lamin's read method here later
+        }
+
+        else if (args.length == 2) {
+            String fileName = args[0];
+            String key = args[1];
+            System.out.println("Reading file name: " + fileName + " using key: " + key);
+            //Call Lamin's key method here later
+        }
+
+        else {
+            System.out.println("Error: invalid command");
+        }
+    }
 }
