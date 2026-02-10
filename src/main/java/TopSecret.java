99cc37c6f9cb0cd9b1ef7060974459e66184f433
AshishSunkarapalli <ashishs5709@gmail.com>
2026-02-09 22:21:16 -0500

    Created the basic outline of the command line code.

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -5,7 +5,7 @@ public class TopSecret {
 
     public static void main(String[] args) {
 
-        if(args.length = 0) {
+        if(args.length == 0) {
             System.out.println("Listing all files...");
             //Call Lamin's list method later
         }
